40cf07c5b461280ad6dd47b11af29a2886cf5ad4
Ahmed Zaki <ahmed.zaki0263@gmail.com>
2025-01-03 19:28:19 +0200

    feat(resources): add users resource.

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,6 +3,9 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+
+use Filament\Forms\Components\DateTimePicker;
+use Filament\Forms\Components\TextInput;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -35,6 +38,41 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'created_at' => 'datetime',
             'password' => 'hashed',
+        ];
+    }
+
+    public static function getForm(): array
+    {
+        return [
+            TextInput::make('name')
+                ->label('User Name')
+                ->string()
+                ->required()
+                ->min(3)
+                ->max(255),
+
+            TextInput::make('email')
+                ->label('Email Address')
+                ->email()
+                ->required()
+                ->maxLength(255)
+                ->unique(ignoreRecord: true),
+
+            DateTimePicker::make('email_verified_at')
+                ->label('Email Verified At')
+                ->default(now()),
+
+            TextInput::make('password')
+                ->label('Password')
+                ->password()
+                ->required(fn(Page $livewire): bool => $livewire instanceof CreateRecord)
+                ->dehydrated(fn($state) => filled($state)),
+
+            TextInput::make('passwordConfirmation')
+                ->label('Password Confirmation')
+                ->password()
+                ->dehydrated(false)
+
         ];
     }
 }
